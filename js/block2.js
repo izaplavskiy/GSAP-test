0a058fc2d36140d8e5c522ed7ec2eb224c64d0ca
i_zaplavskiy <i.zaplavskiy@gmail.com>
2026-03-19 16:13:04 +0200

    normal proj state

--- js/block2.js
+++ js/block2.js
@@ -1,9 +1,9 @@
-// const wrapper = document.getElementById('maskedWrapper')
-// const blockTop = document.getElementById('blockTop')
-// const blockBot = document.getElementById('blockBottom')
-// const slideEls = document.querySelectorAll('.slide')
-// const titleEl = document.getElementById('slideTitle')
-// const textEl = document.getElementById('slideText')
+const wrapper = document.getElementById('maskedWrapper')
+const blockTop = document.getElementById('blockTop')
+const blockBot = document.getElementById('blockBottom')
+const slideEls = document.querySelectorAll('.slide')
+const titleEl = document.getElementById('slideTitle')
+const textEl = document.getElementById('slideText')
 
 let current = 0
 let timer
